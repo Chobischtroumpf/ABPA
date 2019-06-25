485349292febd768ba7ce9fd69ed9d01975de7a9
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-06-25 11:31:49 +0200

    front end landing page mostly done, except for search

--- app/controllers/practiciens_controller.rb
+++ app/controllers/practiciens_controller.rb
@@ -2,8 +2,18 @@ class PracticiensController < ApplicationController
 skip_before_action :authenticate_user!, only: :index
 
   def index
-    @practiciens = Practicien.all
-    @practiciens = policy_scope(Practicien).order(created_at: :desc)
+        if params[:search_query].present?
+      sql_query = " \
+      practiciens.localisation @@ :query \
+      AND practiciens.education @@ :query_bis \
+      "
+      @practiciens = policy_scope(Practicien.where(sql_query, query: "%#{params[:search_query]}%", query_bis: "%#{params[:sq_category]}%"))
+      # @filtered_gardens = @gardens.select(category: params[:sq_category])
+      # .where(category: params[:sq_category])
+
+    else
+     @practiciens = policy_scope(Practicien).order(created_at: :desc)
+    end
   end
 
   def new

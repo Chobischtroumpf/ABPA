ec950a3939df3d53b01b9c692e241bd2f6a38d1f
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-06-30 14:34:18 +0200

    added some images

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,13 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!
   def home
   end
+
+  def quisommesnous
+  end
+
+  def contact
+  end
+
+  def actes
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :practiciens
   resources :agendas 
+  get '/quisommesnous', to: 'pages#quisommesnous', as: :quisommesnous
+  get '/actes', to: 'pages#actes', as: :actes
+  get '/contact', to: 'pages#contact', as: :contact
+  get '/home', to: 'pages#home', as: :home
+
 end

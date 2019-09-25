7d0af3ba26101eea8aeeacc1b46be7e702d12127
alessandro dorigo <dorigo.alessandro@gmail.com>
2019-09-25 14:48:58 +0200

    added conception de la pratique

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'pages/aboutus'
   get 'pages/actes'
   get 'actes/index'
+  get 'pages/conception'
+  get 'pages/education'
+  get 'pages/groupe_aide'
+  get 'pages/aide_individuelle'
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :practiciens
